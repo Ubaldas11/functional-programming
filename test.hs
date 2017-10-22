1bc1613bf37a8e0386b5ef96df4297372ed90f6b
Ubaldas11 <jurgis.kargaudas@gmail.com>
2017-10-22 22:09:11 +0300

    Maybe and Either added

--- test.hs
+++ test.hs
@@ -9,41 +9,49 @@ data Move = Move {
     mark :: Char
 } deriving Show
 
-message = "d1:cd1:0i0e1:1i0ee2:id27"
+message = "d1:cccd1:0i1e1:1i0ee2:id14:TwqkxOFmwqdSCK4:prevd1:cd1:0i2e1:1i0ee2:id2:IY4:prevd1:cd1:0i2e1:1i1ee2:id14:TwqkxOFmwqdSCK4:prevd1:cd1:0i1e1:1i2ee2:id2:IY4:prevd1:cd1:0i2e1:1i2ee2:id14:TwqkxOFmwqdSCK1:v1:oe1:v1:oe1:v1:xe1:v1:oe1:v1:xe"
 
 instance Eq Move where
     (Move x1 y1 id1 mark1) == (Move x2 y2 id2 mark2) =
         (x1 == x2) && (y1 == y2)
 
-w :: String -> String
-w "de" = "The board is empty"
-w str = 
+winner :: String -> Either String (Maybe String)
+winner "de" = Left "ERROR: No moves made"
+winner str = 
     let 
         moves = parseMoves [] str
     in 
-        if (areMovesUnique moves) 
-        then getWinner moves
-        else "There are overlapping moves"        
+        case moves of 
+                Left msg -> Left msg
+                Right moves ->
+                                if (areMovesUnique moves) 
+                                then Right (getWinner moves)
+                                else Left "ERROR: There are overlapping moves"   
 
-parseMoves :: [Move] -> String -> [Move]
-parseMoves moves [] = moves
+             
+parseMoves :: [Move] -> String -> Either String [Move]
+parseMoves moves [] = Right moves
 parseMoves moves rest = 
     let 
-        (move, rest1) = parseMove rest
+        move = parseMove rest
     in
-        parseMoves (move:moves) rest1
+        case move of
+            Left msg -> Left msg
+            Right move -> parseMoves ((fst move):moves) (snd move)
+
         
-parseMove :: String -> (Move, String)
+parseMove :: String -> Either String (Move, String)
 parseMove ('d':rest) = 
     let
         (x, y, restXY) = readCoords rest
         (id, restId) = readId restXY
-        (mark, nextMoveString) = readMark restId
+        mark = readMark restId
     in 
-        (Move x y id mark, nextMoveString)
-parseMove _ = error "Wrong start of move message"
+        case mark of
+            Left msg -> Left msg
+            Right mark -> Right (Move x y id (fst mark), snd mark)
+parseMove _ = Left "ERROR: Wrong start of move message"
 
--- VALIDATE COORDS INPUT
 readCoords :: String -> (Int, Int, String)
 readCoords ('1':':':'c':'d':'1':':':'0':'i':'0':'e':'1':':':'1':'i':'0':'e':'e':rest) = (0,0, rest)
 readCoords ('1':':':'c':'d':'1':':':'0':'i':'0':'e':'1':':':'1':'i':'1':'e':'e':rest) = (0,1, rest)
@@ -54,57 +62,59 @@ readCoords ('1':':':'c':'d':'1':':':'0':'i':'1':'e':'1':':':'1':'i':'2':'e':'e':
 readCoords ('1':':':'c':'d':'1':':':'0':'i':'2':'e':'1':':':'1':'i':'0':'e':'e':rest) = (2,0, rest)
 readCoords ('1':':':'c':'d':'1':':':'0':'i':'2':'e':'1':':':'1':'i':'1':'e':'e':rest) = (2,1, rest)
 readCoords ('1':':':'c':'d':'1':':':'0':'i':'2':'e':'1':':':'1':'i':'2':'e':'e':rest) = (2,2, rest)
-readCoords _ = error "Wrong X and Y format"
+readCoords _ = (-1, -1, "ERROR: wrong coords format")
 
---TODO: read should be rewritten to readMaybe
 readId :: String -> (String, String)
 readId ('2':':':'i':'d':rest) = 
     let
         (idLengthStr, restIdLength) = getIdLengthAsString [] rest
         idLength = parseNumber 0 1 idLengthStr
     in 
-        getId [] idLength restIdLength
-readId _ = error "Wrong id format"
+        case idLength of
+            -1 -> ("ERROR: ID length is not a number", "ERROR: ID length is not a number")
+            _ -> getId [] idLength restIdLength
+readId _ = ("ERROR: wrong ID format", "ERROR: wrong id format")
 
---Catch empty string after colon
 getIdLengthAsString :: String -> String -> (String, String)
-getIdLengthAsString str [] = error "String is empty after ID length"
+getIdLengthAsString str [] =("ERROR: String is empty after ID length", "ERROR: String is empty after ID length")
 getIdLengthAsString str (':':rest) = (str, rest)
 getIdLengthAsString str rest = getIdLengthAsString (str ++ [head rest]) (tail rest)
 
 getId :: String -> Int -> String -> (String, String)
+getId _ _ [] = ("ERROR: ID is empty", "ERROR: ID is empty")
 getId id 0 rest = (id, rest)
 getId id length rest = getId (id ++ [head rest]) (length-1) (tail rest)
 
 --TODO: something smarter for second part of the f-ion?
 --catch wrong format
-readMark :: String -> (Char, String)
+readMark :: String -> Either String (Char, String)
 readMark ('4':':':'p':'r':'e':'v':rest) = getMark (reverse rest)
 readMark ('1':rest) = getMark (reverse ('1':rest))
-readMark _ = error "Wrong format after ID"
+readMark msg = Left msg
 
-getMark :: String -> (Char, String)
-getMark ('e':'x':':':'1':'v':':':'1':rest) = ('X', reverse rest)
-getMark ('e':'X':':':'1':'v':':':'1':rest) = ('X', reverse rest)
-getMark ('e':'o':':':'1':'v':':':'1':rest) = ('O', reverse rest)
-getMark ('e':'O':':':'1':'v':':':'1':rest) = ('O', reverse rest)
-getMark _ = error "Wrong mark format"
+getMark :: String -> Either String (Char, String)
+getMark ('e':'x':':':'1':'v':':':'1':rest) = Right ('X', reverse rest)
+getMark ('e':'X':':':'1':'v':':':'1':rest) = Right ('X', reverse rest)
+getMark ('e':'o':':':'1':'v':':':'1':rest) = Right ('O', reverse rest)
+getMark ('e':'O':':':'1':'v':':':'1':rest) = Right ('O', reverse rest)
+getMark _ = Left "ERROR: Wrong mark format"
 
 areMovesUnique :: [Move] -> Bool
 areMovesUnique [] = True
 areMovesUnique (m:moves) = m `notElem` moves && areMovesUnique moves
 
-getWinner :: [Move] -> String
+
+getWinner :: [Move] -> Maybe String
 getWinner moves = 
     let 
         (xMoves, oMoves) = getPlayersMoves 'X' moves
         xWon = (findThreeInColumns 0 xMoves) || (findThreeInRows 0 xMoves) || (findThreeDiagonally xMoves)
         oWon = (findThreeInColumns 0 oMoves) || (findThreeInRows 0 oMoves) || (findThreeDiagonally oMoves)
     in
-        if xWon then pId (head xMoves)
-        else if oWon then pId (head oMoves)
-        else "Nobody won"
-        -- (xWonColumns, xWonRows, xWonDiagonal, oWonColumns, oWonRows, oWonDiagonal)
+        case (xWon, oWon) of
+            (True, _) -> Just (pId (head xMoves))
+            (_, True) -> Just (pId (head oMoves))
+            (_, _) -> Nothing
 
 getPlayersMoves :: Char -> [Move] -> ([Move], [Move])
 getPlayersMoves char moves = partition (\move -> mark move == char) moves
@@ -131,7 +141,7 @@ findThreeDiagonally moves =
         (fm center && fm botLeft && fm topRight) || (fm center && fm topLeft && fm botRight)
 
 parseNumber :: Int -> Int -> String -> Int
-parseNumber 0 1 [] = error "Id length is missing"
+parseNumber 0 1 [] = -1 -- ERROR parsing number
 parseNumber num i [] = num
 parseNumber num i rest =
     let
@@ -146,4 +156,4 @@ parseNumber num i rest =
                       '7' -> parseNumber (num + 7 * i) (i*10) (init rest)
                       '8' -> parseNumber (num + 8 * i) (i*10) (init rest)
                       '9' -> parseNumber (num + 9 * i) (i*10) (init rest)
-                      _ -> error "Id length is not a number"
+                      _ -> -1 -- ERROR parsing number
