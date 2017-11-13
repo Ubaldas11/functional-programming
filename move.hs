cf227038b90469eec33d789c1b89ba1ac0f841f2
Ubaldas11 <jurgis.kargaudas@gmail.com>
2017-11-13 23:03:43 +0200

    Start of AI (put move into lane with 1 empty slot, one 'O' and one 'X')

--- move.hs
+++ move.hs
@@ -1,6 +1,9 @@
 import Parser
 import MoveDataType
+import Winner
 import Control.Monad
+import Data.List
+
 import Debug.Trace
 import Data.Either
 
@@ -22,13 +25,7 @@ evenMoveNumber moves = Right ((length moves) `mod` 2 == 0)
 
 attack :: [Move] -> Char -> (Int, Int, Char)
 attack [] mark = (1, 1, mark)
-attack moves mark = 
-    let
-        lastMove = last moves
-        x' = mirrorCoord (x lastMove)
-        y' = mirrorCoord (y lastMove)
-    in
-        (x', y', mark)
+attack moves mark = (mirrorCoord (x (last moves)), mirrorCoord (y (last moves)), mark)
 
 mirrorCoord :: Int -> Int
 mirrorCoord 0 = 2
@@ -36,7 +33,57 @@ mirrorCoord 1 = 1
 mirrorCoord 2 = 0
 
 defend :: [Move] -> Char -> (Int, Int, Char)
-defend _ _ = (999, 999, 'D')
+defend allMoves myMark = 
+    let
+        (myMoves, oppMoves) = distributeMovesByMark myMark allMoves
+        emptyMoves = board \\ allMoves
+        moveInSameLane = findMoveInSameLane myMoves oppMoves emptyMoves
+    in
+        case moveInSameLane of
+            Nothing -> (5, 5, myMark)
+            Just value -> (fst value, snd value, myMark)
+
+
+findMoveInSameLane :: [Move] -> [Move] -> [Move] -> Maybe (Int, Int)
+-- neradom savo ejimo, kuris savam lane turetu priesininko ejima
+findMoveInSameLane [] oppMoves emptyMoves = Nothing
+findMoveInSameLane myMoves oppMoves emptyMoves = 
+    let 
+        myMove = head myMoves
+        oppMoveInAxisX = find (\mo -> x mo == x myMove) oppMoves
+        oppMoveInAxisY = find (\mo -> y mo == y myMove) oppMoves
+        nextTry = findMoveInSameLane (tail myMoves) oppMoves emptyMoves
+    in
+        case (oppMoveInAxisX, oppMoveInAxisY) of
+            (Nothing, Nothing) -> nextTry
+            (oppMoveX, oppMoveY) -> 
+                let
+                    foundEmptySpot = findEmptySpot myMove oppMoveX oppMoveY emptyMoves
+                in
+                    case foundEmptySpot of
+                        Nothing -> nextTry
+                        Just value -> foundEmptySpot
+
+findEmptySpot :: Move -> Maybe Move -> Maybe Move -> [Move] -> Maybe (Int, Int)
+findEmptySpot myMove Nothing Nothing emptyMoves = Nothing
+findEmptySpot myMove (Just oppMoveX) oppMoveY emptyMoves =
+    let
+        emptyMove = find(\m -> x m == x oppMoveX) emptyMoves
+    in 
+        case emptyMove of
+            Nothing -> findEmptySpot myMove Nothing oppMoveY emptyMoves
+            Just value -> Just (x value, y value)
+findEmptySpot myMove Nothing (Just oppMoveY) emptyMoves = 
+    let
+        emptyMove = find(\m -> y m == y oppMoveY) emptyMoves
+    in
+        case emptyMove of
+            Nothing -> Nothing
+            Just value -> Just (x value, y value)
+
+board = [Move 0 0 "E" 'E', Move 1 0 "E" 'E', Move 2 0 "E" 'E',
+         Move 0 1 "E" 'E', Move 1 1 "E" 'E', Move 2 1 "E" 'E',
+         Move 0 2 "E" 'E', Move 1 2 "E" 'E', Move 2 2 "E" 'E']
 
 t :: String -> Either String (Int, Int, Char)
 t str = 
