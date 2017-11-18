552941ca85a875c851b72865495688cb1c8b93ba
Ubaldas11 <jurgis.kargaudas@gmail.com>
2017-11-18 12:36:14 +0200

    simple HTTP Get implemented

--- tictactoe/src/Lib.hs
+++ tictactoe/src/Lib.hs
@@ -4,6 +4,18 @@ module Lib
     ) where
 
 import qualified Data.Text.IO as T
+import Network.HTTP as HTTP
+import Network.Stream
+import Debug.Trace
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    getResult <- simpleHTTP (getRequest "http://hackage.haskell.org/")
+    let parsedBody = parseGetStr getResult
+    putStrLn parsedBody
+
+parseGetStr :: Result (Response String) -> String
+parseGetStr (Right value) = rspBody value
+parseGetStr (Left _) = "Conn error occured"
+
+--simpleHTTP :: HStream ty => Request ty -> IO (Result (Response ty))
